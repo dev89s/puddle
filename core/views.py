eb93fa8b008851d04dfcd733af5230966c60f451
Sasan Moshirabadi <sasan.dev89@gmail.com>
2023-10-08 16:45:20 +0300

    Add tailwindcss config, details view & template, related items. Update urls

--- core/views.py
+++ core/views.py
@@ -4,7 +4,7 @@ from item.models import Category, Item
 
 # Create your views here.
 def index(request):
-  items = Item.objects.filter(is_sold=False)[0:6]
+  items = Item.objects.filter(is_sold=False).order_by('category')[0:6]
   categories = Category.objects.all()
   return render(request, 'core/index.html', {
     'categories': categories,
--- puddle/urls.py
+++ puddle/urls.py
@@ -19,11 +19,8 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-from core.views import index, contact
-
 urlpatterns = [
-    path('', index, name='index'),
-    path('contacts/', contact, name='contact'),
+    path('', include('core.urls')),
     path('items/', include('item.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

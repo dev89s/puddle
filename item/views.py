3a5a16ac03b9c8975f9c98bfbb4ac34ab0427be8
Sasan Moshirabadi <sasan.dev89@gmail.com>
2023-10-10 02:34:27 +0300

    Add delete & edit

--- item/views.py
+++ item/views.py
@@ -1,8 +1,8 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 
 from .models import Item
-from .forms import NewItemForm
+from .forms import NewItemForm, EditItemForm
 
 # Create your views here.
 def details(request, pk):
@@ -23,6 +23,7 @@ def new(request):
       item = form.save(commit=False)
       item.created_by = request.user
       item.save()
+      return redirect('/')
   else:
     form = NewItemForm()
 
@@ -30,3 +31,28 @@ def new(request):
     'form': form,
     'title': 'New Item',
   })
+
+@login_required
+def edit(request, pk):
+  item = get_object_or_404(Item, pk=pk, created_by=request.user)
+
+  if request.method == 'POST':
+    form = EditItemForm(request.POST, request.FILES, instance=item)
+
+    if form.is_valid():
+      item.save()
+      return redirect('/')
+  else:
+    form = EditItemForm(instance=item)
+
+  return render (request, 'item/form.html', {
+    'form': form,
+    'title': 'Edit Item',
+  })
+
+@login_required
+def delete(request, pk):
+  item = get_object_or_404(Item, pk=pk, created_by=request.user)
+  item.delete()
+
+  return redirect('dashboard:index')
